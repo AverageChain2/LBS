b25967762cced139e287ca897e6541cb7078be19
Caden Jones <caden.jones@bt.com>
2025-07-29 16:28:51 +0100

    Start of application

---
+++ src/test/java/staffs/lbs/LbsApplicationTests.java
@@ -0,0 +1,13 @@
+package staffs.lbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LbsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
